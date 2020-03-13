64ab298fda500d3c274152a952327c39d1b78981
Sergey Sorokin <sxrxkn@mail.ru>
2020-03-14 00:33:25 +0300

    calc_v2

--- calc_v1/src/calc_v1.c
+++ calc_v1/src/calc_v1.c
@@ -2,24 +2,28 @@
  ============================================================================
  Name        : project_01.c
  Author      : Sxrxkn
- Version     : 1.0
+ Version     : 2.0
  Copyright   :
  Description : A calculator that can work with float numbers with operations as
                multiplication'*', division'/', addition'+', subtraction'-',
-               exponentiation'^',factorial'!'.
+               exponentiation'^',factorial'!' and vectors with operation as
+               addition'+', subtraction'-', scalar multiplication(*).
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-int d,v;
-float a,b,res,factorial,resu;
+int d,v,size;
+float a,b,res,factorial,resu,summ,*e,*l,*resul;
 char c,r,m;
 int main(int argc,char *argv[]) {
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
     r='y';
     while(r=='y'){ // Cycle to repeat the program
+    printf("For operations on vectors, enter (v), for operations on numbers , enter(n): ");
+    scanf(" %c",&c);
+    if(c == 'n'){
         do { // Cycle for correct operation input
             puts("Enter the correct operation(-+/^!*): \n");
             scanf(" %c",&c);
@@ -62,7 +66,57 @@ int main(int argc,char *argv[]) {
                     if(b !=0)
                         printf("Result %f (operation %c)",a/b,c);
                     else printf("No solution");// If divide by 0
-                    break; }}
+                    break; }} }
+    else if(c=='v'){ // If we work with vectors
+        printf("Enter an operation on vectors(+-*): ");
+        scanf(" %c",&c);
+        e=malloc(size*sizeof(int));
+        l=malloc(size*sizeof(int));
+        resul=malloc(size*sizeof(int));
+        if (c == '+'){ // Operation +
+            printf("Enter vector's size: ");
+            scanf(" %i",&size);
+            printf("Enter first vector: ");
+            for(int i=0;i<size;i++) // Cycle for entering the first vector
+                scanf(" %f",&e[i]);
+            printf("Enter second vector: ");
+            for(int i=0;i<size;i++) // Cycle for entering the second vector
+                scanf(" %f",&l[i]);
+            printf("Result vectors addition: ");// Cycle for for performing the operation +
+            for(int i=0;i<size;i++)
+                printf("%f ",e[i]+l[i]);
+            printf("\n");}
+        else if (c == '-'){
+            printf("Enter vector's size: ");
+            scanf(" %i",&size);
+            printf("Enter first vector: ");
+            for(int i=0;i<size;i++)
+                scanf(" %f",&e[i]);
+            printf("Enter second vector: ");
+            for(int i=0;i<size;i++)
+                scanf(" %f",&l[i]);
+            printf("Result vectors subtraction : ");
+            for(int i=0;i<size;i++)// Cycle for for performing the operation -
+                printf("%f ",e[i]-l[i]);
+            printf("\n");}
+        else if (c == '*'){
+            printf("Enter vector's size: ");
+            scanf(" %i",&size);
+            printf("Enter first vector: ");
+            for(int i=0;i<size;i++)
+                scanf(" %f",&e[i]);
+            printf("Enter second vector: ");
+            for(int i=0;i<size;i++)
+                scanf(" %f",&l[i]);
+            printf("Result vectors multiplication: ");
+            for(int i=0;i<size;i++) // Cycle for for performing the operation *
+                summ=summ + e[i]*l[i];
+            printf("%f",summ);}
+        else if (c!='*'|| c!="+" || c!="-") // If entered invalid operation
+            printf("Invalid operation entered");
+        free(e);
+        free(l);
+        free(resul);}
         printf("Continue? (y/n)");// Ability to repeat the program
         scanf(" %c",&r);}
     return 0;}
