78e6e44fc1a51ee401f4cfaf979546d235cbc10c
Sergey Sorokin <sxrxkn@mail.ru>
2020-06-05 11:53:53 +0300

    Add calculator with stack

--- Calculator with stack/src/Calculator with stack.c
+++ Calculator with stack/src/Calculator with stack.c
@@ -1,41 +1,40 @@
 /*
  ============================================================================
- Name        : Calculator with queue
+ Name        : Calculator with stack
  Author      : sxrxkn
  Version     : 1.0
  Copyright   :
- Description : This is a queue calculator implemented via a stack. It is implemented
- to work in Polish notation. It supports operations on float numbers (with operations as
+ Description : This is a calculator with stack implemented in Polish notation.
+ It supports operations on real numbers (with operations as
  multiplication'*', division'/', addition'+', subtraction'-',
  exponentiation'^',factorial'!'). To read multiple lines from a file,
- use the 'n' split character. For example:
+ put the n character at the end of the line.
+ For example :
 2 4 + n
 3 ! n
-10.5 5.5 / n
-2 3 ^
+10.5 5.5 /
  ============================================================================
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-// It if queue
+
 typedef  struct Queue{
     char symbols;
     struct Queue *next;
 }queue;
 
-//It is stack
 typedef struct numbers {
     float value;
     struct numbers *next;
 } stack;
 
 
-// Functions prototypes
+
 void put_queue(char numb);
-char *get_element(void);
+char *getElement(void);
 void push(stack **head_stack, float value);
-float operation_on_numbers_queue(char op, float num1, float num2);
+float operation_on_numbers(char op, float num1, float num2);
 float pop(stack **head_stack);
 
 
@@ -43,7 +42,7 @@ float pop(stack **head_stack);
 
 queue *head = NULL, *tail = NULL;
 stack *head_stack = NULL;
-// Main block
+
 int main(int argc,char *argv[]) {
     setvbuf(stdout, NULL, _IONBF, 0);
     setvbuf(stderr, NULL, _IONBF, 0);
@@ -51,24 +50,24 @@ int main(int argc,char *argv[]) {
     int i = 0, counter = 0;
     char end = 'n', inputfile[100], outputfile[100], tmp, tmp_numb[20], op ='-';
     float num, num1, num2;
-    while(end == 'n'){ // Repeat the program
+    while(end == 'n'){
             printf("Enter name of input file:");
             scanf(" %s", inputfile);
             printf("Enter name of output file:");
             scanf(" %s", outputfile);
             input = fopen(inputfile, "r");
-            while(!feof(input)){ // Read while the file is not finish
+            while(!feof(input)){
                 fscanf(input, "%c", &tmp);
-                if(tmp != '\n'){ // Excluding the line transition character
-                    if(tmp == ' ') // Changing spaces to '_' to separate characters
+                if(tmp != '\n'){
+                    if(tmp == ' ')
                         tmp = '_';
                     put_queue(tmp);
                     i += 1;}}
             fclose(input);
             output = fopen(outputfile, "w");
-            while(i != 1){ // Works until all characters are used
-                tmp_numb[counter] = *get_element();
-                if (tmp_numb[counter] == '_'){ // Converting a string to a number and putting it on the stack
+            while(i != 1){
+                tmp_numb[counter] = *getElement();
+                if (tmp_numb[counter] == '_'){
                     if (op == '-') {
                         tmp_numb[counter] = ' ';
                         num = strtof(tmp_numb, NULL);
@@ -80,27 +79,27 @@ int main(int argc,char *argv[]) {
                     }
                     else
                         i -=1;}
-                else if(tmp_numb[counter] == '!'){ // Perform the operation and write it to the file
+                else if(tmp_numb[counter] == '!'){
                     num1 = pop(&head_stack);
                     num2 = 0;
-                    push(&head_stack,operation_on_numbers_queue(tmp_numb[counter], num1 , num2));
-                    fprintf(output, "%f %c = %f -> ", num1, tmp_numb[counter], operation_on_numbers_queue(tmp_numb[counter], num1 , num2));
+                    push(&head_stack,operation_on_numbers(tmp_numb[counter], num1 , num2));
+                    fprintf(output, "%f %c = %f -> ", num1, tmp_numb[counter], operation_on_numbers(tmp_numb[counter], num1 , num2));
                     i -= 1;
                     counter = 0;
                     op = '+';}
                 else if(tmp_numb[counter] =='*' || tmp_numb[counter] =='+' || tmp_numb[counter]=='-' || tmp_numb[counter] =='/' || tmp_numb[counter]=='^'){
-                    if(head_stack == NULL) // Perform the operations and write it to the file
+                    if(head_stack == NULL)
                         break;
                     num2 = pop(&head_stack);
                     if(head_stack == NULL)
                         break;
                     num1 = pop(&head_stack);
-                    push(&head_stack, operation_on_numbers_queue(tmp_numb[counter], num1, num2));
-                    fprintf(output, "%f %c %f = %f -> ", num1, tmp_numb[counter], num2, operation_on_numbers_queue(tmp_numb[counter], num1 , num2));
+                    push(&head_stack, operation_on_numbers(tmp_numb[counter], num1, num2));
+                    fprintf(output, "%f %c %f = %f -> ", num1, tmp_numb[counter], num2, operation_on_numbers(tmp_numb[counter], num1 , num2));
                     op ='+';
                     i -= 1;
                     counter=0;}
-                else if(tmp_numb[counter]=='n'){ // Switching to another line
+                else if(tmp_numb[counter]=='n'){
                  while(head_stack != NULL)
                      pop(&head_stack);
                  fprintf(output, "|\n");
@@ -108,20 +107,18 @@ int main(int argc,char *argv[]) {
                  i -=1;
                  op = '-';
                 }
-                else{  // If we didn't count the whole number
+                else{
                     counter += 1;
                     i -=1;
                 }
                     }
             fprintf(output,"|");
             fclose(output);
-            printf("Do you want to continue? Print n for continue or any button for close");
+            printf("If you want to continue, press 'n', press any button to turn it off:");
             scanf(" %c", &end);}
     return 0;}
 
-//Block with functions
 
-//Put an item in the queue
 void put_queue(char numb){
     queue *tmp = malloc(1 * sizeof(queue));
     tmp->symbols = numb;
@@ -134,8 +131,8 @@ void put_queue(char numb){
         tail = tmp;
     }
 }
-// Get out of the queue
-char *get_element(void){
+
+char *getElement(void){
     if(head!=NULL){
         char *symb;
         symb = &head->symbols;
@@ -145,7 +142,7 @@ char *get_element(void){
         return NULL;
 }
 
-float operation_on_numbers_queue(char op, float num1, float num2)
+float operation_on_numbers(char op, float num1, float num2)
 {
     float result;
     if(op == '!'){ // Operation "!"
@@ -183,7 +180,7 @@ float operation_on_numbers_queue(char op, float num1, float num2)
 }
 
 
-// Put in stack
+
 void push(stack **head_stack, float value) {
     stack *tmp = malloc(sizeof(stack));
     tmp->next = *head_stack;
@@ -192,7 +189,7 @@ void push(stack **head_stack, float value) {
 }
 
 
-// Get from stack
+
 float pop(stack **head_stack) {
     stack *out;
     float value;
